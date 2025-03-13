5e8ecfab0685c355a7df22e2d860c14ef45584a9
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-03-13 14:28:29 +0530

    added react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hompage from './Hompage'
+
+function App() {
+
+  return (
+    <>
+    <Hompage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hompage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hompage = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <h2>This is <span className='name'>Bhavdeep</span></h2>
+      <h2>You can contact me through : <span className='email'>bhvadeepsai@gmail.com </span></h2>
+    </div>
+  )
+}
+
+export default Hompage
